056efc9ef7740b67963e7eb0d0b4e7aad47d7c18
David Royer <droyer01@gmail.com>
2018-03-16 20:18:00 -0400

    Fixed highlightjs to style code properly

--- components/PostContent.vue
+++ components/PostContent.vue
@@ -3,22 +3,31 @@
 </template>
 <script>
 import Marked from 'marked'
-import HighlightJs from 'highlight.js'
+import Highlight from 'highlight.js/lib/highlight.js'
+
+import highlightCSS from 'highlight.js/lib/languages/css.js';
+import highlightJS from 'highlight.js/lib/languages/javascript.js';
+
+Highlight.registerLanguage('css', highlightCSS);
+Highlight.registerLanguage('javascript', highlightJS);
+
 export default {
   name: 'post-content',
   computed: {
     content () {
       Marked.setOptions({
-        gfm: true,
-        highlight: function (code) {
-          return HighlightJs.highlightAuto(code).value
-        }
+        gfm: true
       })
       return Marked(this.markdown || '')
     }
   },
   props: {
     markdown: String
+  },
+  mounted() {
+    this.$el.querySelectorAll('pre > code').forEach((el) => {
+      Highlight.highlightBlock(el);
+    });
   }
 }
 </script>
